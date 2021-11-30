fa2e87cd36b2c07301f0f85a8bbcb040841faa16
Windows 10 <ing.giuli@gmail.com>
2021-11-30 23:24:07 +0100

    commit

--- APIAuth/Controllers/AggSmSCL01.cs
+++ APIAuth/Controllers/AggSmSCL01.cs
@@ -48,7 +48,7 @@ namespace ApiAuth.Controllers
         {
 
             var vSmsOut = await uow.Query<APISMS_CL01>()
-                .Where(w => w.CLIENTE == "Engie Servizi SpA" || w.SISTEMA.Contains("EAMSPA") || w.SISTEMA.Contains("EAMSSL"))
+                //.Where(w => w.CLIENTE == "Engie Servizi SpA" || w.SISTEMA.Contains("EAMSPA") || w.SISTEMA.Contains("EAMSSL"))
                    .Where(w => w.ID == id)
                 .Select(s => new
           Smscl01
