bec8ba11557dc4e61de9c57a08060a556ac6faaf
Federico Pranio <federico.pranio@nexusat.it>
2021-12-01 23:27:19 +0100

    unit test

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestLog()
+        {
+            Logger.FileLogger logger = new Logger.FileLogger();
+            logger.Log("test");
+        }
+    }
+}
